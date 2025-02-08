2ebce0f56fe85b94e6238517ff1c5a7209ed0385
Anshul Asati <anshulasati@Anshuls-MacBook-Air.local>
2025-02-08 11:58:02 +0530

    added gitignore file

--- drf_projects/watchmate/watchlist_app/api/views.py
+++ drf_projects/watchmate/watchlist_app/api/views.py
@@ -1,22 +1,39 @@
 from rest_framework.response import Response
 from rest_framework import status
 #from rest_framework.decorators import api_view
-from watchlist_app.models import Movie 
-from watchlist_app.api.serializers import MovieSerializer
+from watchlist_app.models import Watchlist,StreamPlatform
+from watchlist_app.api.serializers import WatchlistSerializer
 from rest_framework.views import APIView
 
 
 #CLASS BASED VIEW(APIView)
 #  this class for complete list
-class MovieListAV(APIView):
+
+class StreamPlatformAV(APIView):
+    def get(self,request):
+        platform = StreamPlatform.objects.all()
+        serializer = StreamPlatformSerializer(platform,many = True)
+        return Response(serializer.data)
+
+    def post(self,request):
+        serializer = StreamPlatformSeroializer(data= request.data)# accessing data through request 
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+
+class WatchListAV(APIView):
     def get(self,request):  #requst:all the data
-        movies = Movie.objects.all()
-        serializer = MovieSerializer(movies,many=True)
+        movies = Watchlist.objects.all()
+        serializer = WatchlistSerializer(movies,many=True)
         return Response(serializer.data)
         
       
     def post(self,request):
-        serializer = MovieSerializer(data=request.data)
+        serializer = WatchlistSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -24,27 +41,27 @@ class MovieListAV(APIView):
             return Response(serializer.errors)
          
       # this class  for specific item   
-class MovieDetailAV(APIView):
+class WatchDetailAV(APIView):
     def get(self,request,pk):
         try:
-            movie = Movie.objects.get(pk=pk)
-        except Movie.DoesNotExist:
+            movie = Watchlist.objects.get(pk=pk)
+        except Watchlist.DoesNotExist:
             return Response({'error':"Movie not found"},status= status.HTTP_404_NOT_FOUND) 
-        serializer= MovieSerializer(movie)
+        serializer= WatchlistSerializer(movie)
         return Response(serializer.data) 
         
     
     def put(self,request,pk):
-        movie = Movie.objects.get(pk=pk)
-        serializer = MovieSerializer(movie,data=request.data)
-        if serializer.is_valid(): # check validation here bcz we get the data from he user
+        movie = Watchlist.objects.get(pk=pk)
+        serializer = WatchlistSerializer(movie,data=request.data)
+        if serializer.is_valid(): # check validass tion here bcz we get the data from he user
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)        
             
     def delete(self,request,pk):
-        movie = Movie.objects.get(pk=pk)
+        movie = Watchlist.objects.get(pk=pk)
         movie.delete()
         return Response(status= status.HTTP_204_NO_CONTENT)
         
